d7af868cdcd4cf3dffed505d8a8ec4220e6549f3
stella <sftamesis13@gmail.com>
2022-10-01 11:32:34 -0700

    complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    //Reset timer back to 10 after it hits 0
+    if(timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false);
+      return; //exit early
+    }
+
+    //Have timer count backwards by 1 ever 1 second:
+    const timerID = setTimeout(() => {
+      setTimeRemaining(timeRemaining => timeRemaining - 1)
+    }, 1000)
+
+    return function () {
+      clearTimeout(timerID)
+    }
+  }, [timeRemaining, onAnswered]) //We want to run the effect every time timeRemaining changes. onAnswered is also a dependency, even though it doesn't change.
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
